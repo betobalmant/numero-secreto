61529417c871de82b7ba816522c96abb2ebcb748
beto <beto.balmant@hotmail.com>
2025-01-20 14:47:13 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+alert('Boas vindas ao jogo do número secreto')
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+setTimeout(() => { //foi adicionado essa linha porque console.log não estava apresentando    os valores
+  while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número ente 1 e ${numeroMaximo}`);
+    if (chute == numeroSecreto) {
+      break;
+    } else {
+      if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+      } else {
+        alert(`O número secreto é maior que ${chute}`);
+      }
+      tentativas++;
+    }
+  }
+
+  // variável para guardar palavra 'tentativa' no singular ou plural
+  let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+  alert(
+    `Isso aí! Voce descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`
+  );
+}, 100); // Adiciona um atraso de 100 milissegundos
