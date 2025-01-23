d223169b0158492978ab7b2dca12b6d0e9c453b2
beto <beto.balmant@hotmail.com>
2025-01-23 10:08:14 -0300

    Inserindo comentarios do aprendizado da aula 02

--- app.js
+++ app.js
@@ -26,3 +26,14 @@ setTimeout(() => { //foi adicionado essa linha porque console.log não estava ap
     `Isso aí! Voce descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`
   );
 }, 100); // Adiciona um atraso de 100 milissegundos
+
+
+// Na aula (02), você aprendeu como:
+// * Baixar uma cópia de um repositório hospedado no GitHub para o seu computador, utilizando o comando git clone;
+// * Realizar alterações no código de um projeto e registrá-las com commits, utilizando os comandos git add e git commit;
+// * Visualizar quais arquivos foram modificados no repositório local, utilizando o comando git status;
+// * Listar os commits realizados no repositório, com dados do autor, data e mensagem de cada commit, utilizando o comando git log;
+// * Visualizar os repositórios remotos linkados com o repositório local, utilizando o comando git remote;
+// * Enviar commits feitos no repositório local para o repositório remoto, utilizando o comando git push;
+// * Baixar commits do repositório remoto para o repositório local, utilizando o comando git pull;
+// * Adicionar uma pessoa como colaboradora em um repositório no GitHub, e também como aceitar um convite de colaboração recebido.
